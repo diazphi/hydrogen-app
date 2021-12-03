8d0a00e429a68533049b901a2e69600730f75705
Philip Diaz <philip.diaz@make8.digital>
2021-12-03 23:37:33 +0800

    updates files

---
+++ shopify.config.js
@@ -0,0 +1,6 @@
+export default {
+  locale: 'en-us',
+  storeDomain: 'carcar-cebu-chicharon.myshopify.com',
+  storefrontToken: '64d4940edddedc65803558ace80ca7ba',
+  graphqlApiVersion: 'unstable',
+};
